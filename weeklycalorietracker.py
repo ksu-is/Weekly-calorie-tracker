0ffe36e895542e8a16c1b2d9cfd4e87178fb7ff7
Noahhark <npayton2@students.kennesaw.edu>
2026-04-28 15:27:38 -0400

    Add total_calories calculation to calorie tracker

--- weeklycalorietracker.py
+++ weeklycalorietracker.py
@@ -2,3 +2,5 @@ print("Welcome to the Weekly Calorie Tracker")
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 calories = []
+
+total_calories = sum(calories)
